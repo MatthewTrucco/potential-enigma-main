2c5363978c67951eec6f216177b7edfa09e61416
MatthewTrucco <87626582+MatthewTrucco@users.noreply.github.com>
2023-08-30 11:23:24 -0400

    Update generateMarkdown.js

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -5,6 +5,43 @@ function renderLicenseBadge(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+---
+![${data.license}](https://img.shields.io/badge/license-${data.license}-green)
+
+## Description
+${data.description}
+
+## Table of Contents
+1. [Installation](#installation)
+2. [Usage](#usage)
+3. [License](#license)
+4. [Contributing](#contributing)
+5. [Tests](#tests)
+6. [Questions](#questions)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 `;
 }
