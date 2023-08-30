89ad336b965de6e68bc85a93173cc794664d2fcf
MatthewTrucco <87626582+MatthewTrucco@users.noreply.github.com>
2023-08-30 11:18:39 -0400

    Update index.js

--- Develop/index.js
+++ Develop/index.js
@@ -57,7 +57,15 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    let content = generateMarkdown(data);
+    fs.writeFile(fileName, content, function (error) {
+        if (error){
+            return console.log(error)
+        }
+        console.log('success')
+    });
+}
 
 // TODO: Create a function to initialize app
 function init() {}
