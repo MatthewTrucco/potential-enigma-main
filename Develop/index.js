6f95111b9510e0d87a2eca5dd362c9cc6aa788a1
MatthewTrucco <87626582+MatthewTrucco@users.noreply.github.com>
2023-08-30 11:05:57 -0400

    Update index.js

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,13 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        message: "What is your GitHub username?",
+        name: "username"
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
