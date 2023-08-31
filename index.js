3da607e4ec4189dc0ae2f6c15ad4423f5470ca27
MatthewTrucco <87626582+MatthewTrucco@users.noreply.github.com>
2023-08-31 12:10:35 -0400

    moving around

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
 // TODO: Include packages needed for this application
 
 const { default: inquirer } = require("inquirer");
